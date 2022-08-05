4f0bec04301a3ad07ad852c3372a56fe38d72bca
gamingflexer <osurve20ece@student.mes.ac.in>
2022-08-05 22:39:00 +0530

    added sample data & routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template , Response , request , jsonify
 from pymongo import MongoClient
 from config import *
+import pandas as pd
+from path import heart_beat_csv,location_csv #sample data for testing
 
 app = Flask(__name__)
 
@@ -12,6 +14,23 @@ spark = db.spark
 @app.route('/')
 def index():
     return render_template('home.html')
+
+#apis
+
+@app.route('/api/heartbeat', methods=['GET'])
+def heartbeat():
+    if request.method == 'GET':
+        data = pd.read_csv(heart_beat_csv)
+        out = data.to_dict(orient='records')
+        return jsonify({'data': out})
+
+@app.route('/api/location', methods=['GET'])
+def location():
+    if request.method == 'GET':
+        data = pd.read_csv(location_csv)
+        out = data.to_dict(orient='records')
+        return jsonify({'data': out})
+
 
 
 if __name__ == "__main__":
---
+++ backend/path.py
@@ -0,0 +1,7 @@
+import os
+
+basepath = os.path.dirname(os.path.realpath(__file__))
+
+heart_beat_csv = os.path.join(basepath,'data', 'heartbeat.csv')
+
+location_csv = os.path.join(basepath,'data', 'location.csv')
