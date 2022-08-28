2a4cd6ab507c30eda69d665e7a042c4d231cec97
gamingflexer <osurve20ece@student.mes.ac.in>
2022-08-28 22:20:03 +0530

    production changes reverted

--- backend/static/js/map.js
+++ backend/static/js/map.js
@@ -1,13 +1,4 @@
-
-let mapcordi = [];
-// let x = mapcordi;
-  for (let i = 0; i < 11; i++) 
-  {
-  y=[mapcordi[i]['longitude'],mapcordi[i]['latitude']]
-  console.log(mapcordi)
-  }
-
-  var geojson = {
+var geojson = {
     'type': 'FeatureCollection',
     'features': [
         {
@@ -15,25 +6,36 @@ let mapcordi = [];
             'geometry': {
                 'type': 'LineString',
                 'properties': {},
-                'coordinates': [mapcordi]
+                'coordinates': [
+                    [-77.0366048812866, 38.89873175227713],
+                    [-77.03364372253417, 38.89876515143842],
+                    [-77.03364372253417, 38.89549195896866],
+                    [-77.02982425689697, 38.89549195896866],
+                    [-77.02400922775269, 38.89387200688839],
+                    [-77.01519012451172, 38.891416957534204],
+                    [-77.01521158218382, 38.892068305429156],
+                    [-77.00813055038452, 38.892051604275686],
+                    [-77.00832366943358, 38.89143365883688],
+                    [-77.00818419456482, 38.89082405874451],
+                    [-77.00815200805664, 38.88989712255097]
+                ]
             }
         }
     ]
 };
-console.log('btwn',mapcordi)
 
 var map = new maplibregl.Map({
     container: 'map',
     style:
         'https://api.maptiler.com/maps/streets/style.json?key=pSrPGQipuQ3fvmTiyA5p',
-    center: [-77.0366048812866, 38.89873175227713],
+    center: [-77.0214, 38.897],
     zoom: 12
 });
 
 map.on('load', function () {
     map.addSource('LineString', {
         'type': 'geojson',
-        'data': y
+        'data': geojson
     });
     map.addLayer({
         'id': 'LineString',
@@ -50,7 +52,7 @@ map.on('load', function () {
     });
 
     document
-        .getElementById('zoomTo')
+        .getElementById('zoomto')
         .addEventListener('click', function () {
             var coordinates = geojson.features[0].geometry.coordinates;
             var bounds = coordinates.reduce(function (bounds, coord) {
@@ -62,26 +64,3 @@ map.on('load', function () {
             });
         });
 });
-
-// async function fetchData9() {
-//     const url = "https://retoolapi.dev/BysnBZ/data";
-//     const response = await fetch(url);
-//     const datapoints = await response.json();
-//     return datapoints;
-// }
-    
-fetch('https://retoolapi.dev/0ZmVOz/data').then(response => {
-    return response.json();
-  }).then(data => {
-    // Work with JSON data here
-    mapcordi = data;
-    console.log(mapcordi)
-  }).catch(err => {
-    // Do something for an error here
-  });
-
-
-// var z = setInterval(fetchData9(),1000)
-// console.log(z);
-
-
